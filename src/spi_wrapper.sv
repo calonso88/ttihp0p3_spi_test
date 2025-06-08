c0ed870d1d2e1accc5b3d6925e733c9a18150ede
Caio Alonso da Costa <caioalonso@gmail.com>
2025-06-08 17:19:51 +0200

    Update spi_wrapper.sv

--- src/spi_wrapper.sv
+++ src/spi_wrapper.sv
@@ -1,53 +1,50 @@
 /*
- * Copyright (c) 2024 Caio Alonso da Costa
+ * Copyright (c) 2025 Caio Alonso da Costa
  * SPDX-License-Identifier: Apache-2.0
  */
 
 module spi_wrapper #(parameter int NUM_CFG = 8, parameter int NUM_STATUS = 8, parameter int REG_WIDTH = 8) (rstb, clk, ena, mode, spi_cs_n, spi_clk, spi_mosi, spi_miso, config_regs, status_regs);
 
-  input logic rstb;
-  input logic clk;
-  input logic ena;
+  input  logic rstb;
+  input  logic clk;
+  input  logic ena;
 
-  input logic [1:0] mode;
-  input logic spi_cs_n;
-  input logic spi_clk;
-  input logic spi_mosi;
+  input  logic [1:0] mode;
+  input  logic spi_cs_n;
+  input  logic spi_clk;
+  input  logic spi_mosi;
   output logic spi_miso;
 
   output logic [NUM_CFG*REG_WIDTH-1:0] config_regs;
-  input logic [NUM_STATUS*REG_WIDTH-1:0] status_regs;
-
-  // Address width for register bank
-  localparam int NUM_REGS = NUM_CFG+NUM_STATUS;
-  localparam int ADDR_WIDTH = $clog2(NUM_REGS);
-  localparam int ADDR_WIDTH2 = 8;
+  input  logic [NUM_STATUS*REG_WIDTH-1:0] status_regs;
 
   // Auxiliar variables for spi peripheral
   logic spi_wr_rdn;
-  logic [ADDR_WIDTH-1:0] spi_addr;
+  logic [REG_WIDTH-1:0] spi_addr;
   logic [REG_WIDTH-1:0] spi_rdata, spi_wdata;
   logic spi_we;
-  logic [ADDR_WIDTH2-1:0] spi_addr2;
   
-  logic [REG_WIDTH-1:0] config_mem [NUM_CFG];
-  logic [REG_WIDTH-1:0] status_int [NUM_STATUS];
-
   // Auxiliar variables for spi peripheral
   logic i2c_wr_rdn;
-  logic [7:0] i2c_addr;
+  logic [REG_WDITH-1:0] i2c_addr;
   logic [REG_WIDTH-1:0] i2c_rdata, i2c_wdata;
   logic i2c_we;
 
   // Auxiliar variables for interface register bank
   logic wr_rdn;
-  logic [7:0] addr; 
+  logic [REG_WIDTH-1:0] addr; 
   logic [REG_WIDTH-1:0] rdata, wdata;
   logic we;
+
+  localparam int NUM_REGS = NUM_CFG+NUM_STATUS;
+  localparam int ADDR_REG_BANK_W = $clog(NUM_REGS);
+  logic [ADDR_REG_BANK_W-1:0] addr_reg_bank;
   
-  // Serial interface
+  // Tied extra bit off as SPI peripheral only provides 7 address bits
+  assign spi_addr[REG_WIDTH-1] = 1'b0;
+  
+  // SPI peripheral
   spi_peripheral #(
-    .ADDR_W(ADDR_WIDTH),
     .REG_W(REG_WIDTH)
   ) spi_peripheral_i (
     .clk(clk),
@@ -59,7 +56,7 @@ module spi_wrapper #(parameter int NUM_CFG = 8, parameter int NUM_STATUS = 8, pa
     .spi_clk(spi_clk),
     .spi_cs_n(spi_cs_n),
     .wr_rdn(spi_wr_rdn),
-    .addr(spi_addr),
+    .addr(spi_addr[REG_WIDTH-2:0]),
     .rdata(spi_rdata),
     .wdata(spi_wdata),
     .we(spi_we),
@@ -88,68 +85,37 @@ module spi_wrapper #(parameter int NUM_CFG = 8, parameter int NUM_STATUS = 8, pa
   mux #(
     .WIDTH(1+8+8+1)
   ) mux_addr_i (
-    .a({spi_wr_rdn, spi_addr2, spi_wdata, spi_we}),
-    .b({i2c_wr_rdn, i2c_addr,  i2c_wdata, i2c_we}),
+    .a({spi_wr_rdn, spi_addr, spi_wdata, spi_we}),
+    .b({i2c_wr_rdn, i2c_addr, i2c_wdata, i2c_we}),
     .sel(1'b0),
     .dout({wr_rdn, addr,  wdata, we})
   );
 
+  // Only take the address bits required for NUM_CFG+NUM_STATUS registers
+  assign addr_reg_bank = addr[ADDR_REG_BANK_W-1:0];
+  
   reg_bank #(
-    .ADDR_W(8),
-    .REG_W(8)
+    .REG_W(REG_WIDTH),
+    .ADDR_W($clog2(NUM_CFG+NUM_STATUS)),
+    .NUM_CFG(NUM_CFG),
+    .NUM_STATUS(NUM_STATUS)
   ) reg_bank_i (
     .clk(clk),
     .rstb(rstb),
     .ena(ena),
     .wr_rdn(wr_rdn),
-    .addr(addr),
+    .addr(addr_reg_bank),
     .rdata(),
     .wdata(wdata),
     .we(we),
     .ack(),
-    .err()
+    .err(),
+    .rw_regs(config_regs),
+    .ro_regs(status_regs)
   );
 
-  // Temp
-  assign spi_addr2 = spi_addr;
-
-  // Mux to select CFG or Status Register read access
-  // This imposes a limitation that NUM_CFG and NUM_STATUS have to have the same VALUE!
-  assign rdata = (addr[ADDR_WIDTH-1] == 1'b0) ? config_mem[addr[ADDR_WIDTH-2:0]] : status_int[addr[ADDR_WIDTH-2:0]];
+  // Provide read data to both peripherals
   assign spi_rdata = rdata;
   assign i2c_rdata = rdata;
-
-  // Index for reset register array
-  int i;
-
-  // Register write
-  always_ff @(posedge clk or negedge rstb) begin
-    if (!rstb) begin
-      for (i = 0; i < NUM_CFG; i++) begin
-        config_mem[i] <= '0;
-      end
-    end else begin
-      if (ena) begin
-        if (we) begin
-          config_mem[addr[ADDR_WIDTH-2:0]] <= wdata;
-        end
-      end
-    end
-  end
-
-  // Generate variable
-  genvar x, y;
-  // Convert to unpacked array
-  generate for (x = 0; x < NUM_STATUS; x = x + 1) begin
-    assign status_int[x] = status_regs[((x+1)*REG_WIDTH-1) : x*REG_WIDTH];
-  end endgenerate
-  // Convert to 1 dimension packed array
-  generate for (y = 0; y < NUM_CFG; y = y + 1) begin
-    assign config_regs[((y+1)*REG_WIDTH-1) : y*REG_WIDTH] = config_mem[y];
-  end endgenerate
-
-  // Get rid off lint warning
-  wire _unused = &{spi_wr_rdn, i2c_wr_rdn, i2c_addr, i2c_wdata, i2c_we, 1'b0};
-
 
 endmodule
